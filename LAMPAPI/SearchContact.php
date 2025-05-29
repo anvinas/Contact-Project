fc6d3b7f03f6c776e7a0af4d91d607e5b1d8528e
Adrian Holguin <adrhol12@gmail.com>
2025-05-28 21:44:59 -0400

    Fixed search bug/Shaq's headers

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -1,84 +1,74 @@
 <?php
-	session_start();
+session_start();
 
-	if(!isset($_SESSION("userID"))){
-		returnWithError("User not logged in");
-		exit();
-	}
+// CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 
-	$userID = $_SESSION["userID"];
-	$inData = getRequestInfo();
-	$search = trim($inData["search"]);
 
-	$conn = new mysqli("localhost", "Retro", "Reach", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-		exit()
-	} 
-	else
-	{
-		$searchFirstLast = explode (" ", $search);
-		if(count($searchFirstLat) === 2)
-		{
-			$first = "%" . $searchFirstLast[0]  . "%";
-			$last = "%" .$searchFirstLast[1] . "%";
-		
+if (!isset($_SESSION["userID"])) {
+	returnWithError("User not logged in");
+	exit();
+}
 
-		$stmt = $conn->prepare("SELECT FirstName, LastName from Contacts where (FirstName like ? OR Lastname like ?) and UserID=?");
-		$contactName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ssi", $first, $last, $userID);
-		}
-		else 
-		{
-			$name = "%" . $search . "%";
-			$stmt = $conn->prepare("SELECT firstName, lastName, email, phone FROM Contacts WHERE firstName LIKE ? AND LastName LIKE ? and UserID = ?");
-			$stmt->bind_param("ssi", $name, $name, $userID);
-		}
-		$stmt->execute();
-		$result = $stmt->get_result();
+$userID = $_SESSION["userID"];
+$inData = getRequestInfo();
+$search = trim($inData["search"]);
 
-		$contacts = [];
-		
-		while($row = $result->fetch_assoc())
-		{
-			$contacts[] = $row;
-		}
-		
-		if(empty($contacts))
-		{
-			returnWithError( "No Records Found" );
-		}
-		else
-		{
-			returnWithInfoAsJson(["results" => $contacts, "error" => ""]);
-		}
-		
-		$stmt->close();
-		$conn->close();
-	}
+$conn = new mysqli("localhost", "Retro", "Reach", "COP4331");
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+	exit();
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+$searchFirstLast = explode(" ", $search);
+if (count($searchFirstLast) === 2) {
+	$first = "%" . $searchFirstLast[0] . "%";
+	$last  = "%" . $searchFirstLast[1] . "%";
+	$stmt = $conn->prepare("SELECT FirstName, LastName, Email, Phone FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ?) AND UserID = ?");
+	$stmt->bind_param("ssi", $first, $last, $userID);
+} else {
+	$name = "%" . $search . "%";
+	$stmt = $conn->prepare("SELECT FirstName, LastName, Email, Phone FROM Contacts WHERE FirstName LIKE ? AND LastName LIKE ? AND UserID = ?");
+	$stmt->bind_param("ssi", $name, $name, $userID);
+}
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+$contacts = [];
+while ($row = $result->fetch_assoc()) {
+	$contacts[] = $row;
+}
+
+if (empty($contacts)) {
+	returnWithError("No Records Found");
+} else {
+	sendResultInfoAsJson(array("results" => $contacts, "error" => ""));
+}
+
+$stmt->close();
+$conn->close();
+
+
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo json_encode($obj);
+}
+
+function returnWithError($err)
+{
+	sendResultInfoAsJson(array(
+		"results" => [],
+		"error" => $err
+	));
+}
 ?>
